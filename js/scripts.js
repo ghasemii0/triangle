13b5d957d94ba4440242b4b25eff96122e893601
Mustafa <mustafa@Mustafas-MacBook-Pro.local>
2019-03-28 19:13:23 -0700

    compare the sizes for triangle and says whick triangle it is or if it is not a triangle att all

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,28 @@ $(document).ready(function(){
     var firstInput = parseInt($("#input1").val());
     var secondInput = parseInt($("#input2").val());
     var thirdInput = parseInt($("#input3").val());
-    console.log(typeof firstInput);
-    console.log(secondInput);
-    console.log(thirdInput);
+
 
     if(firstInput === secondInput && secondInput === thirdInput ) {
-      console.log("hi");
+      //console.log("hi");
       $("#Equal").show();
-    }
 
+    } else if (firstInput === secondInput || secondInput === thirdInput || firstInput === thirdInput){
+      //console.log("hi");
+      $("#Isos").show();
+
+    } else if ((firstInput + secondInput <= thirdInput) || (firstInput + thirdInput <= secondInput) ||       (secondInput + thirdInput <= firstInput)) {
+      //console.log("hi");
+      $("#NotTri").show();
+
+
+    } else if ((firstInput + secondInput > thirdInput) || (firstInput + thirdInput > secondInput) ||         (secondInput + thirdInput > firstInput)){
+      console.log("hi");
+      $("#Scal").show();
+
+
+    }
 
     event.preventDefault();
   });
-})
+});
